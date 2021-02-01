62360379147cbf27b12e0e2a941d70a50fda443a
dhruv282 <dhruvpatel282.dp@gmail.com>
2021-01-31 21:28:32 -0500

    Fixed cron bug
    - cronjob execution was unable to find the installed python modules
    
    - cron does not use the same environment variables as the user
    
    - manually export the directories to fix this issue

--- execute_wdill.sh
+++ execute_wdill.sh
@@ -1,3 +1,4 @@
+export PATH="/usr/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 echo $(date)
 echo "------------------------------------------"
 cd <WDILL_PATH> && $(which python3) timelapseTwitter.py
